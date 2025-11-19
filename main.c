ff5ce3752e7132ced377f8083937d026d825d7e7
sarasintoyia8-art <sarasintoyia8@gmail.com>
2025-11-19 14:43:13 +0300

    Cleaned main.c: hardcoded numbers for Task1 submission

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    // Hardcoded numbers
     int a = 10;
     int b = 3;
 
-    // Display numbers
     printf("First number: 10\n");
     printf("Second number: 3\n\n");
 
-    // Display results of all operations
     printf("Addition: 10 + 3 = 13\n");
     printf("Subtraction: 10 - 3 = 7\n");
     printf("Multiplication: 10 * 3 = 30\n");
